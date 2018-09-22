bd84f9653ef288e39ade13931ce03fc1ea96dc9f
bogdanim36 <bogdanim36@gmail.com>
2018-09-22 06:58:09 +0300

    commit

---
+++ Api/Base/GenericService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Data;
+
+namespace WebApi.Api.Base
+{
+	public class GenericService <Model, ModelExtended>
+	{
+		private DataAccess dataAccess;
+		private String tableName ;
+		private String identityColumn;
+		public GenericService()
+		{
+			dataAccess = new DataAccess("MySQL", tableName, identityColumn);
+		}
+		public ModelExtended getItem(int id)
+		{
+			ObjectVar par = new ObjectVar { Name = "?" + identityColumn, Value = id };
+			string select = dataAccess.dataAdapter.SelectCommand.ToString();
+			DataTable data = dataAccess.ExecuteQuery(
+				@"SELECT a.*,
+					 b.name as country, b.nameInvariant as countryInvariant
+					FROM `" + this.tableName + @"` a
+					LEFT JOIN `countries` b ON b.id = a.countryId 
+					Where a.id=?", par);
+			if (data.Rows.Count == 0) return null;
+			else return ConvertToItem<ModelExtended>(data.Rows[0]);
+		}
+		private T ConvertToItem<T>(DataRow row) => new T(row);
+		public IEnumerable<ModelExtended> getItems()
+		{
+			DataTable data = dataAccess.ExecuteQuery(
+				@"SELECT a.*,
+					 b.name as country, b.nameInvariant as countryInvariant
+				  FROM `" + this.tableName + @"` a
+				  LEFT JOIN `countries` b ON b.id = a.countryId");
+			return ConvertToItems(data);
+		}
+		private IEnumerable<ModelExtended> ConvertToItems(DataTable dataTable)
+		{
+			foreach (DataRow row in dataTable.Rows)
+			{
+				yield return ConvertToItem(row);
+			}
+		}
+	}
+}
